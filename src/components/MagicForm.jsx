b8d827394a399d178efccd1a138fac053357ccb3
Quabmac <mccormack.jacob@gmail.com>
2024-01-26 19:11:42 +1100

    added Tags

--- src/components/MagicForm.jsx
+++ src/components/MagicForm.jsx
@@ -11,6 +11,7 @@ import Card from "./Card"
 const MagicForm = () => {
   const [data, setData] = useState("")
   const [search, setSearch] = useState("")
+  const [tags, setTags] = useState("")
   const [returnMessage, setReturnMessage] = useState("")
 
   const magicSearch = async () => {
@@ -28,7 +29,6 @@ const MagicForm = () => {
         }
       )
       setData(await response.json())
-      console.log("Magic Search Data:", data) // Handle the response
     } catch (error) {
       console.error("Error fetching social image", error)
     }
@@ -39,7 +39,7 @@ const MagicForm = () => {
       Title: data.pageTitle,
       URL: search,
       ImageURL: data.socialImage,
-      Tags: data.tags || [],
+      Tags: tagGrab(tags) || [],
     }
 
     fetch(`${import.meta.env.VITE_BACKEND_URL}/api/addData`, {
@@ -58,25 +58,42 @@ const MagicForm = () => {
             }, 1000)
           })
         } else {
-          response.json().then((responseData) => {
-            setReturnMessage(responseData.message) // Assuming responseData.message is your error message
-          })
+          setReturnMessage("Something went wrong, try again") // Assuming responseData.message is your error message
         }
       })
       .catch((error) => {
-        console.error("Error:", error)
-        setReturnMessage("An error occurred")
+        setReturnMessage("An error occurred:", error)
       })
+  }
+
+  function tagGrab(str) {
+    const words = str.split(" ") // Split the input string into an array of words
+    const capitalizedWords = words.map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1) // Capitalize the first letter of each word
+    })
+    console.log(capitalizedWords)
+    return capitalizedWords
   }
 
   return (
     <div className="magicSearch">
       <h1>Let's do some magic</h1>
-      <div className="magicSearchWrap">
-        <input type="text" onChange={(e) => setSearch(e.target.value)} />
-        <button className="magicSearch" onClick={magicSearch}>
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-        </button>
+      <div className="magicSearchAndTagsWrap">
+        <div className="magicSearchWrap">
+          <input
+            type="text"
+            placeholder="Enter URL"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button className="magicSearch" onClick={magicSearch}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <input
+          type="text"
+          placeholder="Separate tags by spaces"
+          onChange={(e) => setTags(e.target.value)}
+        />
       </div>
       <div>
         {data.socialImage && data.pageTitle && (
@@ -87,13 +104,22 @@ const MagicForm = () => {
                 Title: data.pageTitle,
                 URL: search,
                 ImageURL: data.socialImage,
-                Tags: data.tags || [],
+                Tags: tagGrab(tags) || [],
               }}
             />
             <button className="magicSearchSubmit">
               <FontAwesomeIcon icon={faPaperPlane} onClick={sendToPush} />
             </button>
-            <h2 className="magicSearchReturnMessage">{returnMessage}</h2>
+            <h2
+              className="magicSearchReturnMessage"
+              style={
+                returnMessage === "Data added successfully"
+                  ? { color: "rgb(0, 226, 0)" }
+                  : { color: "red" }
+              }
+            >
+              {returnMessage}
+            </h2>
           </div>
         )}
       </div>
